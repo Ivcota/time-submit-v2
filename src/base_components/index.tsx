0163f938e5f1c697cb4ca5d09f364fd8cfdacca9
Iverson Diles <ivcotad@gmail.com>
2023-02-18 08:41:55 -0700

    Setup base input + docker setup (#3)
    * docker setup
    
    * base input
    
    * placeholders

--- src/base_components/index.tsx
+++ src/base_components/index.tsx
@@ -1,3 +1,4 @@
 export { Navbar } from "./navigation/Navbar";
 export { CTAButton } from "./Button";
 export { Container } from "./Container";
+export { Input } from "./Input";
--- src/pages/form.tsx
+++ src/pages/form.tsx
@@ -1,9 +1,13 @@
-import { Navbar } from "../base_components";
+import { Container, Input, Navbar } from "../base_components";
 
 const FormPage = () => {
   return (
     <div>
       <Navbar />
+      <Container>
+        <Input label="First Name" className="mt-3" />
+        <Input label="Last Name" className="mt-3" />
+      </Container>
     </div>
   );
 };
