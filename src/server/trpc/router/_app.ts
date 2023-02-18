fec74d3a09bc89fadf74b5338c7f26c9f14e4fbc
Iverson Diles <ivcotad@gmail.com>
2023-02-17 20:23:13 -0700

    simple query built

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,9 +1,11 @@
 import { router } from "../trpc";
 import { authRouter } from "./auth";
 import { exampleRouter } from "./example";
+import { timeCardRouter } from "./timeCard";
 
 export const appRouter = router({
   example: exampleRouter,
+  timeCard: timeCardRouter,
   auth: authRouter,
 });
 
